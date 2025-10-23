523d1f9c95e93bc59a82fd6787c109ad8aff8b40
guohaolian <1040071899@qq.com>
2025-10-23 12:02:07 +0800

    17

--- Javascript/dsg.js
+++ Javascript/dsg.js
@@ -1,7 +1,34 @@
-const list=[1,2,3,4,5];
+/* const list=[1,2,3,4,5];
 
 const callback=item=>{
     item=item+1;
     console.log(item);
 }
-list.forEach(callback);
+list.forEach(callback); */
+/* var arr = ['李白', '杜甫'];
+
+        console.log('函数调用前arr[0]', arr[0]);//李白
+
+        function changArr(b) {
+
+            arr = ['胡歌', '吴彦祖'];
+
+            console.log('函数调用中arr[0]', arr[0]);//胡歌
+
+        }
+        changArr(arr);
+
+        console.log('函数调用后arr[0]', arr[0]);//胡歌 */
+
+
+var arr = ['李白', '杜甫'];
+
+        console.log('函数调用前arr[0]', arr[0]);//李白
+
+        function changArr(b) {
+            b = ['胡歌', '吴彦祖'];
+            console.log('函数调用中arr[0]', b[0]);//胡歌
+        }
+        changArr(arr);
+
+        console.log('函数调用后arr[0]', arr[0]);//胡歌 李白
