b1cb9ddb0fb166b91934aad280a3e010c9cbf5fb
guohaolian <1040071899@qq.com>
2025-10-25 22:17:53 +0800

    19

--- ghd.js
+++ ghd.js
@@ -1,6 +1,6 @@
 function add(...args) {
     let n=args.reduce((a,b)=>a+b,0);
-    return function _add(...rest){
+    return (...rest)=>{
         if(rest.length===0){
             return n;
         }
