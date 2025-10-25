7b907fc1001344d556a5e9c399200c08c5a2ebe1
guohaolian <1040071899@qq.com>
2025-10-25 14:53:02 +0800

    19

---
+++ Javascript/JavaScript window对象2/js/out1.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2019/1/16.
+ */
+
+alert('I am out111111111 js');
---
+++ Javascript/JavaScript window对象2/js/out2.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2019/1/16.
+ */
+
+alert('I am out22222222 js');
