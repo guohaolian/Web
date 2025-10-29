0597a6b49f809612172f5b5c8273bb356bbb8412
guohaolian <1040071899@qq.com>
2025-10-29 18:52:55 +0800

    23

---
+++ 4_Ajax和PHP开发基础/1_PHP基础/02php名词解释.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+$varible1 = 'frank';
+$varible = 'hello world!';
+echo $varible1;
+echo $varible;
+
+
+/*
+		php常量
+		define('CL1', '1383630574x');
+		//CL1 = '00000'; 不合法的错误行为
+		echo CL1;
+	*/
+
+/*
+		php表达式
+		$varible2 = '456'
+		true
+		$userNameListArr[index]++
+	*/
