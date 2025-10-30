7ee63a1c8eea23ce0822689059575a62be74be31
guohaolian <1040071899@qq.com>
2025-10-30 17:00:01 +0800

    24

--- test.js
+++ test.js
@@ -3,3 +3,10 @@ const p2=Promise.resolve(p1);
 console.log(p1);
 console.log(p2);
 console.log(p2===p1); // true
+
+function isOdd(num){    
+    return num%2!==0;
+}
+function isOdd(n){
+    return n%2===1||n%2===-1;   
+}
