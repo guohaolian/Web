d9710d0d2f43e8c5a21dd5a4dfda3bbe4917852b
guohaolian <1040071899@qq.com>
2025-11-07 20:10:25 +0800

    32

--- 9_webpack/config/webpack.prod.js
+++ 9_webpack/config/webpack.prod.js
@@ -1,9 +1,9 @@
 // Node.js的核心模块，专门用来处理文件路径
 const ESLintPlugin = require("eslint-webpack-plugin");
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");// 自动生成html文件
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");// 提取css成单独文件
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");// css压缩
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // 自动生成html文件
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // 提取css成单独文件
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin"); // css压缩
 // 获取处理样式的Loaders
 const getStyleLoaders = (preProcessor) => {
   return [
@@ -33,51 +33,55 @@ module.exports = {
   module: {
     rules: [
       {
-        // 用来匹配 .css 结尾的文件
-        test: /\.css$/,
-        // use 数组里面 Loader 执行顺序是从右到左
-        use: getStyleLoaders(),
-      },
-      {
-        test: /\.less$/,
-        use: getStyleLoaders("less-loader"),
-      },
-      {
-        test: /\.s[ac]ss$/,
-        use: getStyleLoaders("sass-loader"),
-      },
-      {
-        test: /\.styl$/,
-        use: getStyleLoaders("stylus-loader"),
-      },
-      {
-        test: /\.(png|jpe?g|gif|webp)$/,
-        type: "asset",
-        parser: {
-          dataUrlCondition: {
-            maxSize: 10 * 1024, // 小于10kb的图片会被base64处理
+        oneof: [
+          {
+            // 用来匹配 .css 结尾的文件
+            test: /\.css$/,
+            // use 数组里面 Loader 执行顺序是从右到左
+            use: getStyleLoaders(),
           },
-        },
-        generator: {
-          // 将图片文件输出到 static/imgs 目录中
-          // 将图片文件命名 [hash:8][ext][query]
-          // [hash:8]: hash值取8位
-          // [ext]: 使用之前的文件扩展名
-          // [query]: 添加之前的query参数
-          filename: "static/imgs/[hash:8][ext][query]",
-        },
-      },
-      {
-        test: /\.(ttf|woff2?)$/,
-        type: "asset/resource",
-        generator: {
-          filename: "static/media/[hash:8][ext][query]",
-        },
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/, // 排除node_modules代码不编译
-        loader: "babel-loader",
+          {
+            test: /\.less$/,
+            use: getStyleLoaders("less-loader"),
+          },
+          {
+            test: /\.s[ac]ss$/,
+            use: getStyleLoaders("sass-loader"),
+          },
+          {
+            test: /\.styl$/,
+            use: getStyleLoaders("stylus-loader"),
+          },
+          {
+            test: /\.(png|jpe?g|gif|webp)$/,
+            type: "asset",
+            parser: {
+              dataUrlCondition: {
+                maxSize: 10 * 1024, // 小于10kb的图片会被base64处理
+              },
+            },
+            generator: {
+              // 将图片文件输出到 static/imgs 目录中
+              // 将图片文件命名 [hash:8][ext][query]
+              // [hash:8]: hash值取8位
+              // [ext]: 使用之前的文件扩展名
+              // [query]: 添加之前的query参数
+              filename: "static/imgs/[hash:8][ext][query]",
+            },
+          },
+          {
+            test: /\.(ttf|woff2?)$/,
+            type: "asset/resource",
+            generator: {
+              filename: "static/media/[hash:8][ext][query]",
+            },
+          },
+          {
+            test: /\.js$/,
+            exclude: /node_modules/, // 排除node_modules代码不编译
+            loader: "babel-loader",
+          },
+        ],
       },
     ],
   },
@@ -86,7 +90,7 @@ module.exports = {
     new ESLintPlugin({
       // 指定检查文件的根目录
       context: path.resolve(__dirname, "../src"),
-      overrideConfigFile: 'eslint.config.js',
+      overrideConfigFile: "eslint.config.js",
     }),
     new HtmlWebpackPlugin({
       // 以 public/index.html 为模板创建文件
