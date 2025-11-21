0f300fc8297e378e84d3ac168e94b327e2b166c8
guohaolian <1040071899@qq.com>
2025-11-21 20:04:20 +0800

    36

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-const p1=Promise.resolve(1);
-const p2=Promise.resolve(p1);
+const p1 = Promise.resolve(1);
+const p2 = Promise.resolve(p1);
 console.log(p1);
 console.log(p2);
-console.log(p2===p1); // true
+console.log(p2 === p1); // true
 /* 
 function isOdd(num){    
     return num%2!==0;
