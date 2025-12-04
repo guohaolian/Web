9c87321ba4395b5611ff9632b576fe398ced5037
guohaolian <1040071899@qq.com>
2025-12-04 09:01:40 +0800

    39

--- 深赋值.js
+++ 深赋值.js
@@ -3,16 +3,16 @@ const members = [a];
 a = null;
 console.log(members); */
 //使用 JSON.parse(JSON.stringify(obj))
-/* let a = { key: "value", nested: { subKey: "subValue" } };
+let a = { key: "value", nested: { subKey: "subValue" } };
 let b = JSON.parse(JSON.stringify(a)); // 深赋值
 
 b.key = "newKey"; // 修改 b 中的对象属性
-b.nested.subKey = "newSubValue";    // 修改 b 中的嵌套对象属性
+b.nested.subKey = "newSubValue"; // 修改 b 中的嵌套对象属性
 
-console.log(a); // 输出: { key: 'value', nested: { subKey: 'newSubValue' } }
-console.log(b); // 输出: { key: 'newKey', nested: { subKey: 'newSubValue' } } */
+console.log(a); // 输出: { key: 'value', nested: { subKey: 'subValue' } }
+console.log(b); // 输出: { key: 'newKey', nested: { subKey: 'newSubValue' } }
 
-// 使用递归函数手动实现深拷贝
+/* // 使用递归函数手动实现深拷贝
 function deepCopy(obj) {
   if (obj === null || typeof obj !== "object") {
     return obj;
@@ -36,4 +36,4 @@ b.key = "newKey"; // 修改 b 中的对象属性
 b.nested.subKey = "newSubValue"; // 修改 b 中的嵌套对象属性
 
 console.log(a); // 输出: { key: 'value', nested: { subKey: 'subValue' } }
-console.log(b); // 输出: { key: 'newKey', nested: { subKey: 'newSubValue' } }
+console.log(b); // 输出: { key: 'newKey', nested: { subKey: 'newSubValue' } } */
